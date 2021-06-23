5ef07a74a477e3ab25c8e6eaf4e0c5e7c1cb62b0
Isabelly <isabelly.paganini@gmail.com>
2021-06-23 13:08:12 -0300

    Arquitetura de Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,23 +12,18 @@ import localePt from '@angular/common/locales/pt';
 registerLocaleData(localePt);
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './navegacao/menu/menu.component';
-import { HomeComponent } from './navegacao/home/home.component';
-import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
-import { rootRouterConfig } from './app.routes';
+import { AppRoutingModule } from './app.routes';
 import { DataBindingComponent } from './demos/data-binding/data-binding.component';
 import { ProdutoService } from './produtos/produtos.service';
 import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
+import { NavegacaoModule } from './navegacao/navegacao.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    HomeComponent,
-    FooterComponent,
     SobreComponent,
     ContatoComponent,
     DataBindingComponent,
@@ -38,14 +32,15 @@ import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.compo
   ],
   imports: [
     BrowserModule,
-    [RouterModule.forRoot(rootRouterConfig, { useHash: false })],
     FormsModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
     NgBrazil,
     TextMaskModule,
-    CustomFormsModule
+    CustomFormsModule,
+    NavegacaoModule,
+    AppRoutingModule
   ],
   providers: [
     ProdutoService,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 import { DataBindingComponent } from './demos/data-binding/data-binding.component';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
@@ -6,13 +8,26 @@ import { SobreComponent } from './institucional/sobre/sobre.component';
 import { HomeComponent } from './navegacao/home/home.component';
 import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
 
-export const rootRouterConfig: Routes =[
+const rootRouterConfig: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'contato', component: ContatoComponent },
     { path: 'sobre', component: SobreComponent },
     { path: 'feature-data-binding', component: DataBindingComponent },
-    { path: 'produtos', component: ListaProdutoComponent },
+    // { path: 'produtos', component: ListaProdutoComponent },
     { path: 'produto-detalhe/:id', component: ListaProdutoComponent },
-    { path: 'cadastro', component: CadastroComponent}
+    { path: 'cadastro', component: CadastroComponent},
+    { path: 'produtos', 
+            loadChildren: () => import('./demos/arquitetura-componentes/produto.module')
+                .then(m => m.ProdutoModule)},
 ];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(rootRouterConfig)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule { }
